8eaf18367477d2c3ff4146d739bebff14bdb62cd
gabi <gabi.matamu@gmail.com>
2016-06-13 22:05:31 +0200

    Add rooms management feature in admin

--- app/scripts/controllers/adminConsole.js
+++ app/scripts/controllers/adminConsole.js
@@ -1,10 +1,22 @@
 
 'use strict';
 angular.module('calendarApp')
-  .controller('adminConsoleController', function ($scope, $cookieStore, databaseService) {
+  .controller('adminConsoleController', function ($scope, $cookieStore, databaseService, sharedService) {
         $scope.adminToken = '';
         $scope.adminTokenEndTime = '';
-    
+        $scope.error = undefined;
+        $scope.rooms=[];
+
+        $scope.$on('handleBroadcast', function() {
+            var message = sharedService.message;
+            $scope.authToken = message.token;
+        });
+
+        var globalsCookies = $cookieStore.get('globals');
+        if(globalsCookies !== undefined) {
+            $scope.authToken = globalsCookies.token;
+        }
+
         this.generateAdminToken = function() {
             $scope.error = undefined;
             $scope.dataLoading = true;
@@ -26,5 +38,62 @@ angular.module('calendarApp')
             }
             
         };
+
+        $scope.initRooms = function() {
+            $scope.rooms=[];
+            databaseService.getRoomsDB().then(function(data)
+            {
+                var rooms = data.data;
+                for(var i = 0; i < rooms.length; i++){
+                    var newRoom = {
+                        name: rooms[i].room,
+                        oldName: undefined,
+                        isNew: false
+                    };
+                    $scope.rooms.push(newRoom);
+                }
+                $scope.error = undefined;
+            },function(data, status){
+                console.log(status);
+                console.log(data);
+                $scope.error = data.data.error;
+            });
+        };
+
+        this.modifyRoom = function(room){
+            room.oldName = room.name;
+        };
+
+        this.updateRoomInDB = function(room){
+            databaseService.updateRoomDB(room, $scope.authToken).
+                then(function() {
+                    $scope.initRooms();
+                    $scope.messageAdmin = "Salle mise à jour.";
+                }, function(data) {
+                    $scope.error = data.data.error;
+                });
+        };
+
+        this.deleteRoomDB = function(room) {
+            databaseService.deleteRoomDB(room, $scope.authToken).
+                then(function() {
+                    $scope.initRooms();
+                    $scope.messageAdmin = "Salle supprimée.";
+                }, function(data) {
+                    $scope.error = data.data.error;
+                });
+        };
+
+        this.addNewRoom = function() {
+            var newRoom = {
+                name: "Nouvelle salle",
+                oldName: undefined,
+                isNew: true
+            };
+
+            $scope.rooms.push(newRoom);
+        };
+
+
 });
 
