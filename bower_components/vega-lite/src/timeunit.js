a7cbef38247589f65b24e4d19cc7e290a1a2d91d
gabi <gabi.matamu@gmail.com>
2016-07-07 01:01:49 +0200

    remove config file, need to run npm install and bower install to install dependencies

--- bower_components/vega-lite/src/timeunit.js
+++
@@ -1,7 +0,0 @@
-exports.TIMEUNITS = [
-    'year', 'month', 'day', 'date', 'hours', 'minutes', 'seconds', 'milliseconds',
-    'yearmonth', 'yearmonthday', 'yearmonthdate', 'yearday', 'yeardate',
-    'yearmonthdayhours', 'yearmonthdayhoursminutes', 'hoursminutes',
-    'hoursminutesseconds', 'minutesseconds', 'secondsmilliseconds'
-];
-//# sourceMappingURL=timeunit.js.map
--- bower_components/vega-lite/typings/vega.d.ts
+++
@@ -1,4 +0,0 @@
-declare module vg {
-  export var version;
-  export function embed(selector: string, apec: any, f: any);
-}
