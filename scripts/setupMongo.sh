ecbe55845585cd3e350686623dd4e77ea7f6803c
gabi <gabi.matamu@gmail.com>
2016-08-12 13:12:00 +0200

    Correct mongo setup

--- scripts/setupMongo.sh
+++ scripts/setupMongo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-e
+MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+#MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+#MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
+
+
 echo "Waiting for Mongo startup.."
 until curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
